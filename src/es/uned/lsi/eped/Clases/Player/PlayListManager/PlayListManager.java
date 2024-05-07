095b77fb431aae7f01b9e3921de7ff632f34d3b0
Jordan <ascologo@gmail.com>
2024-05-08 00:16:04 +0200

    Creacion RecentlyPlayed y PlayListManager

--- src/es/uned/lsi/eped/Clases/Player/PlayListManager/PlayListManager.java
+++ src/es/uned/lsi/eped/Clases/Player/PlayListManager/PlayListManager.java
@@ -6,49 +6,94 @@ import es.uned.lsi.eped.pract2023_2024.*;
 
 public class PlayListManager implements PlayListManagerIF {
 
-    private PlayListIF playlists;
+    private ListIF<String> playLists;
+    private ListIF<ListaParejas> lists;
 
     public PlayListManager() {
-        playlists = new PlayList();
+        playLists = new List<String>();
     }
 
     @Override
     public boolean contains(String playListID) {
-        boolean contains = false;
-        this.playlists.getPlayList();
-        
-       return contains;
+        boolean found = false;
+        for (int i = 0; i < this.playLists.size(); i++) {
+            if (this.playLists.contains(playListID)) {
+                found = true;
+            }
+        }
+       return found;
     }
 
     @Override
     public PlayListIF getPlayList(String playListID) {
-        boolean contains = false;
-        this.playlists.getPlayList().iterator();
-        while ( this.playlists.getPlayList().iterator().hasNext()) {
-            if (this.playlists.getPlayList().iterator().getNext().equals(Integer.parseInt(playListID))) {
-                contains =  true;break;
+        PlayListIF play = null;
+        if (contains(playListID)) {
+            for (int i = 0; i < this.lists.size(); i++) {
+                if (this.lists.get(i).getId().equals(playListID)) {
+                    play = this.lists.get(i).getPlayList();
+                    break;
+                }
             }
-       }
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPlayList'");
+        }
+        return play;
     }
 
     @Override
     public ListIF<String> getIDs() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getIDs'");
+        ListIF<String> play = new List<String>();
+        for (int i = 0; i < this.lists.size(); i++) {
+            play.insert(i, this.lists.get(i).getId());
+        }
+        return play;
     }
 
     @Override
     public void createPlayList(String playListID) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createPlayList'");
+        try {
+            if(contains(playListID)) {
+                throw new Exception("Ya existe una lista con ese ID");
+            }
+            else {
+                this.playLists.insert(this.playLists.size(),playListID);
+                this.lists.insert(this.playLists.size(),new ListaParejas(playListID));
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 
     @Override
     public void removePlayList(String playListID) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removePlayList'");
+        try {
+            if(contains(playListID)) {
+                for (int i = 0; i < this.playLists.size(); i++) {
+                    if (this.playLists.get(i).equals(playListID)) {
+                        this.playLists.remove(i);
+                        this.lists.remove(i);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static class ListaParejas  {
+        private String id;
+        private PlayListIF playList;
+
+        public ListaParejas(String id) {
+            this.id = id;
+            this.playList = new PlayList();
+        }
+
+        public String getId() {
+            return this.id;
+        }
+
+        public PlayListIF getPlayList() {
+            return this.playList;
+        }
     }
     
 }
